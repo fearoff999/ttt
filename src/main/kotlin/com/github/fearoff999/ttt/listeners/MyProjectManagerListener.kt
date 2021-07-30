b7491da4c9d834d4d6c9167005a4c1dc32c81dea
GitHub Action <action@github.com>
2021-07-30 20:08:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/fearoff999/ttt/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/fearoff999/ttt/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fearoff999.ttt.listeners
 
+import com.github.fearoff999.ttt.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/fearoff999/ttt/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fearoff999/ttt/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fearoff999.ttt.services
 
+import com.github.fearoff999.ttt.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
